e2fcc1bbdc575fe1e74c9ae7b1b23f08cece4580
IsmailofficialGithub <ismail.official295@gmail.com>
2025-09-09 21:47:25 +0500

    wrap / page in LayoutWrapper

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,20 @@
 import HeroSection from "./components/HeroSection";
-import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
-import Footer from "./components/Footer";
 import AchievementsSection from "./components/AchievementsSection";
 import Blogsection from "./components/Blogsection";
+import LayoutWrapper from "@/lib/LayoutWrapper";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
-      <Navbar />
-      <div className="container mt-24 mx-auto px-12 py-4">
-        <HeroSection />
+   <LayoutWrapper>
+     <HeroSection />
         <AchievementsSection />
         <AboutSection />
         <ProjectsSection />
         <Blogsection />
         <EmailSection />
-      </div>
-      <Footer />
-    </main>
+   </LayoutWrapper>
   );
 }
