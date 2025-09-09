28f14c3ffe8a57d4cf98c79f7eb18ad15d0943ec
IsmailofficialGithub <ismail.official295@gmail.com>
2025-09-09 21:49:38 +0500

    create folder for /project route

---
+++ src/app/projects/page.jsx
@@ -0,0 +1,13 @@
+import LayoutWrapper from '@/lib/LayoutWrapper'
+import React from 'react'
+import ProjectShowcase from '../components/ProjectShowcase'
+
+const Projects = () => {
+  return (
+    <LayoutWrapper>
+            <ProjectShowcase/>
+    </LayoutWrapper>
+  )
+}
+
+export default Projects
