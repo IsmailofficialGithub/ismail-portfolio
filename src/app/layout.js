f4162c47eaa89ad2f3a8c82d7d2bb3664e63e830
IsmailofficialGithub <ismail.official295@gmail.com>
2025-09-10 22:22:44 +0500

    fixed toast

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
-import SessionRapper from './helper/providerRapper'
+import {Toaster} from 'react-hot-toast'
 import { SessionProvider } from 'next-auth/react'
 export const metadata = {
   title: 'Ismail Portfolio',
@@ -13,7 +13,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <SessionProvider>
-        <body className={inter.className}>{children}</body>
+     
+        <body className={inter.className}>
+             <Toaster/>
+          {children}
+          </body>
       </SessionProvider>
     </html>
   )
