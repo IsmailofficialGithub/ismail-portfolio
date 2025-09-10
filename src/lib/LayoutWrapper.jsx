f4f78d35f831a7314aaa93b0dd69e62494fcd00d
IsmailofficialGithub <ismail.official295@gmail.com>
2025-09-10 11:06:17 +0500

    giving inline css paddingX

--- src/lib/LayoutWrapper.jsx
+++ src/lib/LayoutWrapper.jsx
@@ -5,7 +5,7 @@ const LayoutWrapper = ({ children }) => {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
-      <div className={`container mt-24 mx-auto px-12 py-4`}>{children}</div>
+      <div className="container mt-24 mx-auto pl-12 pr-12 py-4" style={{paddingLeft:"3rem",paddingRight:"3rem"}}>{children}</div>
       <Footer />
     </main>
   );
