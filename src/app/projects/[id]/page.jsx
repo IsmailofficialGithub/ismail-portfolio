fdbccc690c194edba5c34480e665815cd78b58ac
IsmailofficialGithub <ismail.official295@gmail.com>
2025-09-11 19:04:34 +0500

    fixed imports , wrap page with layout wrapper

---
+++ src/app/projects/[id]/page.jsx
@@ -0,0 +1,12 @@
+import ProjectDetailComponent from "@/app/components/ProjectDetailComponent"
+import LayoutWrapper from "@/lib/LayoutWrapper"
+
+const ProjectDetail = () => {
+  return (
+    <LayoutWrapper>
+        <ProjectDetailComponent/>
+    </LayoutWrapper>
+  )
+}
+
+export default ProjectDetail
