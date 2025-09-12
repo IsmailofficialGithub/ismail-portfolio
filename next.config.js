ddeca3cd4bfc1d274634ae119b9246cafa273c47
IsmailofficialGithub <ismail.official295@gmail.com>
2025-09-12 10:45:55 +0500

    ignore ESLint errors during builds

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
      images: {
           domains: ['res.cloudinary.com',"images.unsplash.com"], // Allow images from Cloudinary
      },
+      eslint: {
+    // ✅ ignore ESLint errors during builds
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
